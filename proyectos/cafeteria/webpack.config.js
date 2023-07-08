6cf88aa050153912134da9f83d3e1ef7f9795e21
Cristian Pan <a16003566@alumnos.uady.mx>
2023-07-08 12:19:50 -0600

    seccion 19: avances
    videos: 199 - 220

---
+++ proyectos/cafeteria/webpack.config.js
@@ -0,0 +1,92 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require("autoprefixer");
+const CopyPlugin = require("copy-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const ImageWebpMin = require("imagemin-webp-webpack-plugin");
+const ImageAvifMin = require("imagemin-avif-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  entry: "./src/js/app.js",
+  watch: true,
+  output: {
+    filename: "js/app.bundler.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer()],
+              },
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/app.bundle.css",
+    }),
+   /* new CopyPlugin({
+      patterns: [
+        {
+          from: "src/img",
+          to: "img",
+        },
+      ],
+    }),
+    new ImageminPlugin({
+      test: /\.(jpe?g|png)$/i,
+      pngquant: {
+        quality: "60-80",
+      },
+      jpegtran: {
+        progressive: true,
+      },
+      destination: path.resolve(__dirname, "build", "img"),
+    }),
+    new ImageAvifMin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 75,
+          },
+  
+        },
+      ],
+    }),
+    new ImageWebpMin({
+      config: [
+        {
+          test: /\.(jpe?g|png)/,
+          options: {
+            quality: 75,
+          },
+        },
+      ],
+    }), */
+  ],
+};
